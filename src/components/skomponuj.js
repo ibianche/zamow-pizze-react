8af5e1c2c82ac8b422a3ad99500b1f0a631e78c2
ibianche <inna.pomorska@gmail.com>
2022-07-28 14:24:33 +0200

    wyswietlamy liste dodatkow na stronie z pliku dane.json f28

--- src/components/skomponuj.js
+++ src/components/skomponuj.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Dodatki from '../dane';
 
 
 function Skomponuj() {
+
+  const [ dodatki, setDodatki ] = useState([]);
+
+  useEffect( () => {
+
+    }, []);     //[] dodaje sie po to zeby metoda byla wykonana tylko 1 raz
+
+
   return (
     <div>
       <h1>Skomponuj pizzę </h1>
+      { Dodatki.map((dodatek) => {  //pobieramy elementy z dane.json   dodatek tworzymi sami to jest kazdy element tablicy
+        return(
+          <p>{dodatek.nazwa}</p>
+        )
+      })}
     </div>
   );
 }
